ab151ca872c41be1c09443edcd18cca1fa2a54ff
abdullox <abdulloxislomov39@gmail.com>
2024-07-26 12:19:38 +0500

    books serializers

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -12,9 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    language_name = serializers.CharField(source='language.name', read_only=True)
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('title', 'category_name', 'language_name', 'scan')
 
 
 class DiscussSerializer(serializers.ModelSerializer):
